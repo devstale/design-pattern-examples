6266e335a689a60fa468b9c8ba311cb9a9313718
devstale <devstale@alice.it>
2020-05-02 16:58:46 +0200

    'commit'

--- src/com/devstale/pattern/exception/GitException.java
+++ src/com/devstale/pattern/exception/GitException.java
@@ -1,5 +1,10 @@
 package com.devstale.pattern.exception;
 
+/**
+ * 
+ * @author DEVS_tale
+ *
+ */
 public class GitException extends Exception {
 
 	private static final long serialVersionUID = -3836114440915586316L;
