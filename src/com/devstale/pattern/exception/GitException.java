e6379b229ece6c4a508fef6e73a9e2c85fdc76b5
devstale <devstale@alice.it>
2020-05-02 16:38:28 +0200

    prova builder

---
+++ src/com/devstale/pattern/exception/GitException.java
@@ -0,0 +1,12 @@
+package com.devstale.pattern.exception;
+
+public class GitException extends Exception {
+
+	private static final long serialVersionUID = -3836114440915586316L;
+
+	public GitException(String message) {
+		super(message);
+	}
+
+
+}
